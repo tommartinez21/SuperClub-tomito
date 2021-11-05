ef38fd6ff4782bea815204e392f0357e89b01866
tommartinez21 <tommartinez@santandertecnologia.com.ar>
2021-11-05 11:59:17 -0300

    Coreccion paths

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const app = express();
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.render('index'))
+app.get('/', (req, res) => res.render('pages/index'))
 
-app.get('/product/:id', (req, res) => res.render('product'))
+app.get('/product/:id', (req, res) => res.render('pages/product'))
 
-app.get('/carat', (req, res) => res.render('cart'))
+app.get('/cart', (req, res) => res.render('pages/cart'))
 
-app.get('/checkout', (req, res) => res.render('checkout'))
+app.get('/checkout', (req, res) => res.render('pages/checkout'))
 
-app.get('/register', (req, res) => res.render('register'))
+app.get('/register', (req, res) => res.render('pages/register'))
 
-app.get('/login', (req, res) => res.render('login'))
+app.get('/login', (req, res) => res.render('pages/login'))
 
 app.listen(3000, () => console.log('toy ready'))
